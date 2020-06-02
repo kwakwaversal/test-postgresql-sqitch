cef546801dc65c64df92be2c775c648e7cf769fc
Paul Williams <kwakwaversal@gmail.com>
2020-06-02 23:07:49 +0100

    Add support for tables with compound PKs

--- deploy/trigger/audit/log_changes.sql
+++ deploy/trigger/audit/log_changes.sql
@@ -7,25 +7,33 @@ CREATE OR REPLACE FUNCTION audit.log_changes()
   RETURNS trigger AS
 $BODY$
 -- Only *updates* to a table are tracked in `audit.table_changes`. This allows
--- rudimentary PITR (point in time recovery) to be performed on tables which
--- match a specific convention.
+-- rudimentary PITR (point in time recovery) to be performed on tables.
 --
--- N.B., this will not work for composite primary keys.
+-- For this to work, the table *can have* an `updated_at` column to extract the
+-- change time (the name of the column can be overridden). If you set this
+-- option to `null` it will assume there is no such column and default to
+-- `CURRENT_TIMESTAMP`.
+--
+-- N.B., this will not work as expected if you change the value of the primary
+-- key - you would have to work to figure out it changing from one PK to a new
+-- PK.
 --
 -- @param {jsonb} args - options
--- @param {text} [args.pk] - optional primary key to use (default: `id`)
 -- @param {text} [args.exclude] - regular expression of columns to exclude.
 -- e.g., '^added_(at|by)$' will not include the column `added_at` or
 -- `added_by`. Useful for excluding columns that change all the time but aren't
 -- important to the auditing log such as the `updated_at` column.
+-- @param {text} [args.pk] - optional primary key to use (default: `ARRAY['id']`)
+-- @param {text} [args.updated_by] - optional column name to extract the
+-- "change" time from the record (default: `updated_at`)
 --
 -- @example
 -- CREATE TEMPORARY TABLE foo (
 --   id uuid default public.gen_random_uuid() not null,
 --   name text,
 --   num integer,
---   added_at timestamp with time zone default CURRENT_TIMESTAMP,
---   added_by uuid
+--   created_at timestamp with time zone default CURRENT_TIMESTAMP,
+--   created_by uuid
 -- );
 --
 -- CREATE TRIGGER foo_trigger
@@ -38,12 +46,37 @@ DECLARE
     WHEN TG_NARGS = 1 THEN TG_ARGV[0]::JSONB
   END;
   col text;
+  colexclude text := CASE
+    WHEN args#>'{exclude}' IS NOT NULL THEN args->>'exclude'
+    ELSE '^(updated)_(at|by)$'
+  END;
   jsnew jsonb := to_jsonb(NEW);
   jsold jsonb := to_jsonb(OLD);
-  pkcol text := COALESCE(args->>'pk', 'id');
+  pkcol text [] := COALESCE(
+    NULLIF(
+      ARRAY(
+        SELECT
+          jsonb_array_elements_text(args->'pk')
+      ),
+      '{}'
+    ),
+    ARRAY['id']
+  );
+  pkval text[];
   upcol text := COALESCE(args->>'updated_by', 'updated_by');
 BEGIN
   IF TG_OP = 'UPDATE' AND TG_LEVEL = 'ROW' THEN
+
+    -- Get the primary key value. This function supports compound indexes and
+    -- stores them as an array - so this builds up the `pkval` array.
+    FOREACH col IN ARRAY pkcol
+    LOOP
+      IF (jsnew->col)::text IS NULL THEN
+        RAISE EXCEPTION 'No primary key `%` found for `%.%` when inserting audit log', col, TG_TABLE_SCHEMA, TG_TABLE_NAME;
+      END IF;
+
+      pkval = pkval || (jsnew->col)::text;
+    END LOOP;
 
     FOR col IN
       SELECT
@@ -51,7 +84,7 @@ BEGIN
       FROM
         jsonb_object_keys(jsnew) key
       WHERE
-        (args->>'exclude' IS NULL OR key !~ (args->>'exclude')::text)
+        (colexclude IS NULL OR LOWER(key) !~ colexclude)
     LOOP
       IF jsnew->>col IS DISTINCT FROM jsold->>col THEN
         INSERT INTO
@@ -72,7 +105,7 @@ BEGIN
             TG_TABLE_NAME,
             col,
             pkcol,
-            jsnew->>pkcol,
+            pkval,
             jsold->>col,
             jsnew->>col
           );
--- test/trigger/audit/log_changes.sql
+++ test/trigger/audit/log_changes.sql
@@ -1,6 +1,5 @@
 BEGIN;
 
-/* SELECT plan(1); */
 SELECT no_plan();
 
 /****************************************************************************/
@@ -22,11 +21,10 @@ PREPARE reset_audit_changes AS
 -- Basic test
 
 CREATE TEMPORARY TABLE basic (
-  id uuid default public.gen_random_uuid() not null,
+  id serial,
   name text,
   num integer,
-  added_at timestamp with time zone default CURRENT_TIMESTAMP,
-  added_by uuid
+  created_at timestamp with time zone default CURRENT_TIMESTAMP
 );
 
 CREATE TRIGGER basic_trigger
@@ -35,10 +33,39 @@ CREATE TRIGGER basic_trigger
     EXECUTE PROCEDURE audit.log_changes();
 
 INSERT INTO basic(name) VALUES('hello');
-EXECUTE audit_count(0, 'insert does not create table_changes');
+EXECUTE audit_count(0, 'inserts do not create table_changes records');
 
-UPDATE basic SET name='bar', added_at=NOW()+'1 second'::interval, num=10;
-EXECUTE audit_count(3, '3 changes added from update');
+UPDATE
+  basic
+SET
+  name = 'bar',
+  num = 10,
+  -- Slightly contrived test - you shouldn't really be changing the create_at
+  -- or created_by but this would be logged as it is useful.
+  created_at = NOW() + '1 second' :: interval;
+EXECUTE audit_count(3, '3 changes added from update (name, num, created_at)');
+
+EXECUTE reset_audit_changes;
+
+/*****************************************************************************/
+-- Can override default `exclude` of `^updated_(at|by)$`
+
+CREATE TEMPORARY TABLE overrideexclude (
+  id serial not null,
+  name text,
+  updated_at timestamp with time zone default CURRENT_TIMESTAMP,
+  updated_by text
+);
+
+CREATE TRIGGER overrideexclude_trigger
+  AFTER UPDATE ON overrideexclude
+    FOR EACH ROW
+    EXECUTE PROCEDURE audit.log_changes('{"exclude": null}');
+
+INSERT INTO overrideexclude(name) VALUES('hello');
+UPDATE overrideexclude SET name = 'foo', updated_by = 'you';
+SELECT is(count(*)::integer, 2, 'can disable exclude (name, updated_at)')
+  FROM audit.table_changes;
 
 EXECUTE reset_audit_changes;
 
@@ -48,18 +75,17 @@ EXECUTE reset_audit_changes;
 CREATE TEMPORARY TABLE exclusion (
   id serial not null,
   name text,
-  added_at timestamp with time zone default CURRENT_TIMESTAMP,
-  added_by uuid
+  foo text
 );
 
 CREATE TRIGGER exclusion_trigger
   AFTER UPDATE ON exclusion
     FOR EACH ROW
-    EXECUTE PROCEDURE audit.log_changes('{"exclude": "^added_at$"}');
+    EXECUTE PROCEDURE audit.log_changes('{"exclude": "^foo$"}');
 
 INSERT INTO exclusion(name) VALUES('hello');
-UPDATE exclusion SET name='bar', added_at=NOW()+'1 second'::interval;
-EXECUTE audit_count(1, '2 changes added from update');
+UPDATE exclusion SET name='bar', foo='bar';
+EXECUTE audit_count(1, '1 change added from update');
 
 EXECUTE reset_audit_changes;
 
@@ -72,18 +98,49 @@ CREATE TEMPORARY TABLE addedby (
   added_at timestamp with time zone default CURRENT_TIMESTAMP,
   added_by text,
   updated_at timestamp with time zone default CURRENT_TIMESTAMP,
-  updated_by text
+  updated_by_is_different text
 );
 
 CREATE TRIGGER addedby_trigger
   AFTER UPDATE ON addedby
     FOR EACH ROW
-    EXECUTE PROCEDURE audit.log_changes('{"updated_by": "updated_by"}');
+    EXECUTE PROCEDURE audit.log_changes('{"updated_by": "updated_by_is_different"}');
 
 INSERT INTO addedby(name) VALUES('hello');
-UPDATE addedby SET name='bar', updated_at=NOW()+'1 second', updated_by='eric';
-SELECT is(count(*)::integer, 3, 'eric is the owner of 3 updates')
+UPDATE
+  addedby
+SET
+  name = 'bar',
+  updated_at = NOW() + '1 second',
+  updated_by_is_different = 'eric';
+SELECT is(count(*)::integer, 2, 'eric is the owner of 2 updates (name, updated_by_is_different)')
   FROM audit.table_changes WHERE added_by = 'eric';
+
+EXECUTE reset_audit_changes;
+
+/*****************************************************************************/
+-- Multicolumn compound index
+
+CREATE TEMPORARY TABLE compoundidx (
+  id_a integer not null,
+  id_b integer not null,
+  name text
+);
+
+-- Multiple primary keys (or primary keys that are not `id`) require the
+-- trigger argument to explicitly list them.
+CREATE TRIGGER compoundidx_trigger
+  AFTER UPDATE ON compoundidx
+    FOR EACH ROW
+    EXECUTE PROCEDURE audit.log_changes('{"pk": ["id_a", "id_b"]}');
+
+INSERT INTO compoundidx(id_a, id_b, name) VALUES(1, 2, 'hello');
+
+UPDATE compoundidx SET name='bar';
+EXECUTE audit_count(1, '1 change for compound index');
+
+SELECT is(count(*)::integer, 1)
+  FROM audit.table_changes WHERE pk_value = ARRAY['1', '2'];
 
 EXECUTE reset_audit_changes;
 
